9248b3e9bb6f613d81312d28cf53173c2c9560b3
LucasPalminio <l.palminio01@ufromail.cl>
2020-09-21 17:29:14 -0300

    Se crearon los paquetes y las clases respectivas.

---
+++ src/main/java/ClienteTCP/ClienteTCP.java
@@ -0,0 +1,7 @@
+package ClienteTCP;
+
+public class ClienteTCP {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ServidorTCP/ServidorTCP.java
@@ -0,0 +1,9 @@
+package ServidorTCP;
+/*
+* Monitorear desde el cliente, la hora de servidor, incluidos segundos y milisegundos.
+* */
+public class ServidorTCP {
+    public static void main(String[] args) {
+
+    }
+}
