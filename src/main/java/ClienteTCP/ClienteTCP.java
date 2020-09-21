0ad26fc25768d5637cb5b65fea018cae65800ab2
LORENZO <l.devia02@ufromail.cl>
2020-09-21 18:09:57 -0300

    Se agregó la estructura principal de las clases ClienteTCP y ServidorTCP, ademas en ServidorTCP se creó el reloj requerido.

--- src/main/java/ClienteTCP/ClienteTCP.java
+++ src/main/java/ClienteTCP/ClienteTCP.java
@@ -1,7 +1,19 @@
 package ClienteTCP;
 
+import java.net.*;
+import java.io.*;
+
 public class ClienteTCP {
     public static void main(String[] args) {
-
+        Socket clienteSocket;
+        try{
+            clienteSocket = new Socket("127.0.0.1",50000);
+            do{
+                System.out.println();
+            }while(true);
+        }catch(Exception e){
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
     }
 }
--- src/main/java/ServidorTCP/ServidorTCP.java
+++ src/main/java/ServidorTCP/ServidorTCP.java
@@ -1,9 +1,38 @@
 package ServidorTCP;
+
+import java.net.*;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 /*
 * Monitorear desde el cliente, la hora de servidor, incluidos segundos y milisegundos.
 * */
 public class ServidorTCP {
     public static void main(String[] args) {
+        LocalDateTime reloj = LocalDateTime.now();
 
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss:SSS");
+
+        String tiempoActual = reloj.format(formato);
+
+        System.out.println("Hello world");
+
+        System.out.println(tiempoActual);
+
+        ServerSocket servidorHora;
+
+        try{
+            servidorHora = new ServerSocket(50000);
+
+            Socket horaSocket = servidorHora.accept();
+            do{
+                reloj = LocalDateTime.now();
+                tiempoActual = reloj.format(formato);
+            }while(true);
+        }catch(Exception e){
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
     }
 }
