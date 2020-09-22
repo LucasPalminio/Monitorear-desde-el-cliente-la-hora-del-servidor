1b40b60a3ac33fe7e2a94f43f07094adf92cb9df
LucasPalminio <l.palminio01@ufromail.cl>
2020-09-22 17:46:53 -0300

    *Se esta elaborando un menu por el lado del cliente

--- src/main/java/ClienteUDP/ClienteUDP.java
+++ src/main/java/ClienteUDP/ClienteUDP.java
@@ -1,9 +1,25 @@
 package ClienteUDP;
 
 import java.net.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ClienteUDP {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenido al solicitador de hora (cliente)");
+        int nroSolicitudes;
+        while(true) {
+            try {
+                System.out.print("¿Cuantas solicitudes desea hacer?: ");
+                nroSolicitudes = scanner.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.err.println("Error, ¡ingrese un numero! "+e.getMessage());
+                scanner.nextLine();
+            }
+        }
+
         Socket clienteSocket;
         try{
             clienteSocket = new Socket("127.0.0.1",50000);
