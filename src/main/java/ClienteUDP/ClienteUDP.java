12ef02e34d73ad71f416772994aaf5befad355a5
LORENZO <l.devia02@ufromail.cl>
2020-09-23 18:29:30 -0300

    Se agregó un SocketTimeoutException.

--- src/main/java/ClienteUDP/ClienteUDP.java
+++ src/main/java/ClienteUDP/ClienteUDP.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.net.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
 
 public class ClienteUDP {
     public static void main(String[] args) {
@@ -31,18 +32,23 @@ public class ClienteUDP {
                 DatagramPacket paquete = new DatagramPacket(mensaje_bytes, mensaje.length(), address, 6000);
                 clienteSocket.send(paquete);
                 int contador = 0;
-                do {
+                clienteSocket.setSoTimeout(100);
+                try {
+                    do {
 
-                    byte[] recogerServidor_bytes = new byte[256];
-                    DatagramPacket servPaquete = new DatagramPacket(recogerServidor_bytes, 256);
-                    clienteSocket.receive(servPaquete); // Se recibe la respuesta
-                    String cadenaMensaje = new String(recogerServidor_bytes).trim(); // Se almacena la respuesta en una variable String
-                    if (!cadenaMensaje.equals("")) {
-                        System.out.println(cadenaMensaje); // Se imprime la hora recibida del servidor
-                        contador++;
-                    }
-                } while (contador < nroSolicitudes);
-
+                        byte[] recogerServidor_bytes = new byte[256];
+                        DatagramPacket servPaquete = new DatagramPacket(recogerServidor_bytes, 256);
+                        clienteSocket.receive(servPaquete); // Se recibe la respuesta
+                        String cadenaMensaje = new String(recogerServidor_bytes).trim(); // Se almacena la respuesta en una variable String
+                        if (!cadenaMensaje.equals("")) {
+                            System.out.println(cadenaMensaje); // Se imprime la hora recibida del servidor
+                            contador++;
+                        }
+                    } while (contador < nroSolicitudes);
+                }catch(SocketTimeoutException e){
+                    System.out.println("No se ha podido conectar al servidor");
+                    clienteSocket.close();
+                }
             } catch (InputMismatchException e) { // Este mensaje de error se despliega en caso de no introducir un número para enviar, pero no afecta a la continuidad misma del programa
                 System.err.println("Error, ¡ingrese un numero! " + e.getMessage());
                 scanner.nextLine();
